bd138771f586db1d1978a2e70d4b813f71fde5e8
Alexandre de Aguiar <alexandre.ffsis@gmail.com>
2023-11-20 20:27:11 -0300

    correção do getAllPosts

--- src/useCases/posts/getAllPosts.js
+++ src/useCases/posts/getAllPosts.js
@@ -2,36 +2,17 @@ import Post from "../../entities/posts/Post.js";
 
 async function getAllPosts(req, res) {
     try {
-        const postId = req.params.id;
-        const postList = await Post.find().populate("user").limit(10).sort("date");
+        const postList = await Post.find()
+            .select("-__v")
+            .populate({ path: "user", select: "-avatar -avatarContentType -password -__v" })
+            .limit(10)
+            .sort("date");
 
         if (!postList) {
             return res.status(404).json({ message: "Nenhuma Postagem encontrada" });
         }
 
-        let result = [];
-        var resultRowAuthor;
-        var resultRow;
-
-        postList.forEach((row) => {
-            resultRowAuthor = {
-                id: row.user._id.toString(),
-                name: row.user.name,
-                email: row.user.email,
-            };
-
-            resultRow = {
-                title: row.title,
-                date: row.date,
-                category: row.category,
-                text: row.text,
-                author: resultRowAuthor,
-            };
-
-            result.push(resultRow);
-        });
-
-        res.status(200).json(result);
+        res.status(200).json(postList);
     } catch (error) {
         console.log("getAllPosts:", error);
         res.status(500).json({ message: "Erro ao obter as postagens" });
